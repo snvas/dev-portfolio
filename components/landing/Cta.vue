9684717d14f3258d5ed2d984758a54c35d418da8
Soraia Novaes <sonovaes511@gmail.com>
2024-05-08 19:32:25 -0300

    Ajustado: bg cta transparente

--- components/landing/Cta.vue
+++ components/landing/Cta.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="contato"
-	  class="bg-black px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center"
+	  class="light:bg-black dark:bg-transparent px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center"
 	>
 	  <h2 class="text-white text-3xl md:text-6xl">Websites mais rápidos.</h2>
 	  <p class="text-slate-500 mt-4 text-lg md:text-xl">
