78fd9a981cb33dd157dcc8edcc2505d850b4bd55
Soraia Novaes <sonovaes511@gmail.com>
2024-05-08 19:44:38 -0300

    Ajustado: bg color CTA

--- components/landing/Cta.vue
+++ components/landing/Cta.vue
@@ -1,9 +1,9 @@
 <template>
 	<div id="contato"
-	  class="light:bg-black dark:bg-transparent px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center"
+	  class="bg-[#111213] px-20 py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center"
 	>
-	  <h2 class="text-white text-3xl md:text-6xl">Websites mais rápidos.</h2>
-	  <p class="text-slate-500 mt-4 text-lg md:text-xl">
+	  <h2 class="text-white text-3xl md:text-6xl">Websites mais rápidos</h2>
+	  <p class="text-slate-400 mt-4 text-lg md:text-xl">
 		Este portfólio é um testemunho do meu crescimento como desenvolvedora web e da minha paixão por construir soluções web eficazes e atraentes.
 	  </p>
 	  <div class="flex mt-5">
