544fa7630946b9a7e26961ab1faf26ab84645b3a
Soraia Novaes <sonovaes511@gmail.com>
2024-06-03 23:46:52 -0300

    Ajustado: numero whatsapp

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -17,13 +17,14 @@
         Soraia Novaes
       </h1>
       <p class="text-lg mt-4 max-w-xl">
-        Engenheira de Software pela 42 São Paulo.  
-		<wbr /> +10 anos de experiência gerenciando e desenvolvendo projetos de aplicações web com conhecimentos
-        HTML 5, CSS 3, JavaScript,Tailwind, GIT, Docker e WordPress.
+        Engenheira de Software pela 42 São Paulo.
+        <wbr /> +10 anos de experiência gerenciando e desenvolvendo projetos de
+        aplicações web com conhecimentos HTML 5, CSS 3, JavaScript,Tailwind,
+        GIT, Docker e WordPress.
       </p>
       <div class="mt-6 flex flex-col sm:flex-row gap-3">
         <LandingLink
-          href="https://wa.me/5511999247466"
+          href="https://wa.me/5511940062075"
           target="_blank"
           rel="noopener"
           >Entre em Contato</LandingLink
